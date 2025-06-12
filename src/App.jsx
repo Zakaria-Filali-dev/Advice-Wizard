b5a21b405548a24a2d0ebd310a12064e9dae7f77
Zakaria <87984946+Zakaria-Filali-dev@users.noreply.github.com>
2025-06-12 10:09:36 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import TheAdvice from './components/TheAdvice'
+
+function App() {
+
+
+  return (
+    <>
+<TheAdvice/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TheDice.jsx
@@ -0,0 +1,15 @@
+export default function TheDice({ onClick }) {
+    return (
+        <button
+            className="dicebtn"
+            onClick={onClick}
+            style={{ position: 'absolute', left: '50%', bottom: '-28px', transform: 'translateX(-50%)' }}
+        >
+            <img
+                className="diceImg"
+                src="../../images/icon-dice.svg"
+                alt="dice image"
+            />
+        </button>
+    );
+}
